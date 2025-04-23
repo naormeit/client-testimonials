10b8b45cb95c90bec872a14e8d76a4e11dd3fbbc
naormeit <necrospre@gmail.com>
2025-04-23 12:19:15 +0530

    final done

--- client-testimonials/src/App.jsx
+++ client-testimonials/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
       <div>
         <TestimonialCard name="Emily Watson" review="This service was amazing! Highly recommended." />
-        <TestimonialCard name="Raghav Duggal" review="Exceptional quality and prompt delivery!" />
+        <TestimonialCard name="Raghav Duggal" review="Exceptional quality" />
       </div>
   );
 
